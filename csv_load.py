bece8158027c6210ea226442b7488171219e22f6
euthy0 <90341326+euthy0@users.noreply.github.com>
2021-10-22 11:36:40 -0600

    Create csv_load.py

---
+++ csv_load.py
@@ -0,0 +1,10 @@
+
+import csv
+
+file_path = 'C:/Users/codym/OneDrive - University of Calgary/Desktop/Econ 411 - Computer Applications/soccer data/soccer_data.csv'
+
+with open(file_path, 'r', newline = '') as csv_f:
+    data = list(csv.reader(csv_f))
+
+# print headers
+print(data[0])
