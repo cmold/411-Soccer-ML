a8859f066b0064720be5fc5eda7371e6a6c0ddaa
Cody <90341326+euthy0@users.noreply.github.com>
2021-10-29 12:14:36 -0600

    csv load and team ELO + name dictionary

--- csv_load.py
+++ csv_load.py
@@ -6,5 +6,14 @@ file_path = 'C:/Users/codym/OneDrive - University of Calgary/Desktop/Econ 411 - 
 with open(file_path, 'r', newline = '') as csv_f:
     data = list(csv.reader(csv_f))
 
-# print headers 
-print(data[0])
+# Get all the team names and give them a base elo score of 1200    
+teams = {}
+for i in data[1:]:
+    if i[2] not in teams:
+        teams[i[2]] = 1200
+
+for i in data[2:]:
+    if i[2] not in teams:
+        teams[i[2]] = 1200
+
+print(teams)
