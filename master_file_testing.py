addeae3682dc4369188d8c1ff2d3241e68a45212
Cody <90341326+euthy0@users.noreply.github.com>
2021-11-26 11:46:57 -0700

    goal difference

--- master_file_testing.py
+++ master_file_testing.py
@@ -21,32 +21,46 @@ def expected_outcome(ratingA, ratingB):
         return(outcome, 1)
     
 # Function to update two teams ratings after a match
-def rating_update(expected_outcome, actual_outcome, teamA, teamB, kval):
+def rating_update(expected_outcome, actual_outcome, teamA, teamB, kval, goal_difference):
     
-    teamA_rating = (teamA + kval*(actual_outcome - expected_outcome))
-    teamB_rating = (teamB + kval*((1-actual_outcome) - abs(expected_outcome - 1)))
+    teamA_rating = (teamA + (0.9*kval)*(actual_outcome - expected_outcome) + (0.1*kval) * goal_difference)
+    teamB_rating = (teamB + (0.9*kval)*((1-actual_outcome) - abs(expected_outcome - 1)) + (0.1*kval) * goal_difference)
 
     return(teamA_rating, teamB_rating)
 
 def run_games(data, teams):
 
     # Loop through each data point (game)
-    print(len(data[1]))
     for i in data[1:len(data) - 3]:
         home = i[2]
         away = i[3]
         winner = i[6]
 
+        if i[4] == '':
+            goals_home = 0
+        else:
+            goals_home = int(i[4])
+
+        if i[5] == '':
+            goals_away = 0
+        else:
+            goals_away = int(i[5])
+
+        
+        goal_difference = int(goals_home) - int(goals_away)
+
         if winner == '1':
-            results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], 25)
+            results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], 25, goal_difference)
             teams[home] = results[0]
             teams[away] = results[1]
 
         if winner == '0':
-            results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], 25)
+            results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], 25, goal_difference)
             teams[home] = results[1]
             teams[away] = results[0]
         
+
+
     return(teams)
 
 # function to compare 2 teams
