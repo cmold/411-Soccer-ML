a8664a0ad8c27991ff7c9320ef3a5d2375b6dfe1
Cody <90341326+euthy0@users.noreply.github.com>
2021-11-29 16:07:21 -0700

    Update master_file_testing.py

--- master_file_testing.py
+++ master_file_testing.py
@@ -24,15 +24,15 @@ def expected_outcome(ratingA, ratingB):
 def rating_update(expected_outcome, actual_outcome, winner, loser, KVAL, ind_variables):
     
     winner_rating = (winner + (0.6511*KVAL)*(actual_outcome - expected_outcome) + 
-    ((0.2*KVAL) * abs(ind_variables['Goal Difference']))+ 
-    ((0.0453*KVAL) * ind_variables['Foul Difference']) + 
+    ((0.2*KVAL) * ind_variables['Goal Difference'])+ 
+    ((0.0453*KVAL) * ind_variables['Fouls']) + 
     ((0.0611*KVAL) * ind_variables['Shots Taken']) +
     ((0.0425*KVAL) * ind_variables['Corners']))
 
 
     loser_rating = (loser + (0.6511*KVAL)*((1-actual_outcome) - abs(expected_outcome - 1)) + 
     ((0.2*KVAL) * ind_variables['Goal Difference']) + 
-    ((0.0453*KVAL) * ind_variables['Foul Difference']) + 
+    ((0.0453*KVAL) * ind_variables['Fouls']) + 
     ((0.0611*KVAL) * ind_variables['Shots Taken']) +
     ((0.0425*KVAL) * ind_variables['Corners']))
 
@@ -45,7 +45,7 @@ def run_games(data, teams, KVAL, ind_variables):
 
     # Loop through each data point (game)
     # skip last 300 as they are used for testing purposes
-    for i in data[1:len(data) - 300]:
+    for i in data[1:len(data) - 200]:
         home = i[2]
         away = i[3]
         winner = i[6]
@@ -53,17 +53,20 @@ def run_games(data, teams, KVAL, ind_variables):
         goals_home = int(i[4])
         goals_away = int(i[5])
 
-        ind_variables['Corners'] = int(i[17]) - int(i[18])
-        ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
-        ind_variables['Foul Difference'] = int(i[15]) - int(i[16])
-        ind_variables['Goal Difference'] = int(goals_home) - int(goals_away)
-
         if winner == '1':
+            ind_variables['Corners'] = int(i[17]) - int(i[18])
+            ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
+            ind_variables['Fouls'] = int(i[15]) - int(i[16])
+            ind_variables['Goal Difference'] = int(goals_home) - int(goals_away)
             results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], KVAL, ind_variables)
             teams[home] = results[0]
             teams[away] = results[1]
 
         if winner == '0':
+            ind_variables['Corners'] = int(i[18]) - int(i[17])
+            ind_variables['Shots Taken'] = int(i[14]) - int(i[13])
+            ind_variables['Fouls'] = int(i[16]) - int(i[15]) 
+            ind_variables['Goal Difference'] = int(goals_away) - int(goals_home)
             results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], KVAL, ind_variables)
             teams[home] = results[1]
             teams[away] = results[0]
@@ -75,11 +78,11 @@ def run_games(data, teams, KVAL, ind_variables):
 def outcome_pr(teams, home_team, away_team):
 
     # Check the elo of the teams and weight the winner with a range for draws
-    if teams[home_team] > teams[away_team] + 30:
+    if teams[home_team] > teams[away_team] + 50:
         #print(f'{home_team} will win')
         return(home_team)
     
-    elif teams[home_team] < teams[away_team] - 30:
+    elif teams[home_team] < teams[away_team] - 50:
         #print(f'{away_team} will win')
         return(away_team)
     
@@ -95,7 +98,7 @@ def ML_prediction(data, teams, KVAL):
     ind_variables = {}
 
     # Looks at the last 300 games in our data set
-    for i in data[len(data) - 300:]:
+    for i in data[len(data) - 200:]:
         home = i[2]
         away = i[3]
         winner = i[6]
@@ -117,17 +120,20 @@ def ML_prediction(data, teams, KVAL):
         goals_home = int(i[4])
         goals_away = int(i[5])
 
-        ind_variables['Corners'] = int(i[17]) - int(i[18])
-        ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
-        ind_variables['Foul Difference'] = int(i[15]) - int(i[16])
-        ind_variables['Goal Difference'] = int(goals_home) - int(goals_away)
 
         if winner == '1':
+            ind_variables['Corners'] = int(i[17]) - int(i[18])
+            ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
+            ind_variables['Fouls'] = int(i[15]) - int(i[16])
+            ind_variables['Goal Difference'] = int(goals_home) - int(goals_away)
             results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], KVAL, ind_variables)
             teams[home] = results[0]
             teams[away] = results[1]
 
         if winner == '0':
+            ind_variables['Corners'] = int(i[18]) - int(i[17])
+            ind_variables['Shots Taken'] = int(i[14]) - int(i[13])
+            ind_variables['Fouls'] = int(i[16]) - int(i[15])
             results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], KVAL, ind_variables)
             teams[home] = results[1]
             teams[away] = results[0]
