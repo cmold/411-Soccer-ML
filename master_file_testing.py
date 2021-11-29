e30ec99ee7df3bd309d272e45bbd87936384d59d
Cody <90341326+euthy0@users.noreply.github.com>
2021-11-29 12:53:06 -0700

    Run predictions on last 300 games, with correct ratio

--- master_file_testing.py
+++ master_file_testing.py
@@ -21,63 +21,103 @@ def expected_outcome(ratingA, ratingB):
         return(outcome, 1)
     
 # Function to update two teams ratings after a match
-def rating_update(expected_outcome, actual_outcome, teamA, teamB, kval, goal_difference):
+def rating_update(expected_outcome, actual_outcome, teamA, teamB, KVAL, goal_difference):
     
-    teamA_rating = (teamA + (0.9*kval)*(actual_outcome - expected_outcome) + (0.1*kval) * goal_difference)
-    teamB_rating = (teamB + (0.9*kval)*((1-actual_outcome) - abs(expected_outcome - 1)) + (0.1*kval) * goal_difference)
+    teamA_rating = (teamA + (0.9*KVAL)*(actual_outcome - expected_outcome) + (0.1*KVAL) * goal_difference)
+    teamB_rating = (teamB + (0.9*KVAL)*((1-actual_outcome) - abs(expected_outcome - 1)) + (0.1*KVAL) * goal_difference)
 
     return(teamA_rating, teamB_rating)
 
-def run_games(data, teams):
+def run_games(data, teams, KVAL):
 
     # Loop through each data point (game)
-    for i in data[1:len(data) - 3]:
+    # skip last 300 as they are used for testing purposes
+    for i in data[1:len(data) - 300]:
         home = i[2]
         away = i[3]
         winner = i[6]
 
-        if i[4] == '':
-            goals_home = 0
-        else:
-            goals_home = int(i[4])
-
-        if i[5] == '':
-            goals_away = 0
-        else:
-            goals_away = int(i[5])
+        goals_home = int(i[4])
+        goals_away = int(i[5])
 
         
         goal_difference = int(goals_home) - int(goals_away)
 
         if winner == '1':
-            results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], 25, goal_difference)
+            results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], KVAL, goal_difference)
             teams[home] = results[0]
             teams[away] = results[1]
 
         if winner == '0':
-            results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], 25, goal_difference)
+            results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], KVAL, goal_difference)
             teams[home] = results[1]
             teams[away] = results[0]
-        
 
 
     return(teams)
 
 # function to compare 2 teams
-def outcome_pr(teams):
-    team_a = input("Home Team: ")
-    team_b = input("Away Team: ")
+def outcome_pr(teams, home_team, away_team):
 
-    if teams[team_a] > teams[team_b]:
-        print(f'{team_a} will win')
+    if teams[home_team] > teams[away_team]:
+        #print(f'{home_team} will win')
+        return(home_team)
     
-    elif teams[team_a] < teams[team_b]:
-        print(f'{team_b} will win')
+    elif teams[home_team] < teams[away_team]:
+        #print(f'{away_team} will win')
+        return(away_team)
     
     else:
-        print('Draw')
+        return('Draw')
+
+# Function to use our model in predicting soccer game outcomes
+# Takes csv data and a dictionary of teams elo scores as parameters
+def ML_prediction(data, teams, KVAL):
+    correct = 0
+    wrong = 0
+    real_outcome = ''
+
+    # Looks at the last 300 games in our data set
+    for i in data[len(data)-300:]:
+        home = i[2]
+        away = i[3]
+        winner = i[6]
+
+        if winner == '1': real_outcome = home
+        elif winner == '0': real_outcome = away
+        elif winner == '0.5': real_outcome = 'Draw'
+
+        # Get our models predicted winner
+        prediction = outcome_pr(teams, home, away)
+
+        # Keep track of # of correct and wrong
+        if prediction == real_outcome:
+            correct += 1
+        else:
+            wrong += 1
+
+        # The rest of this section adds updates the ELO rating based on the current game
+        goals_home = int(i[4])
+        goals_away = int(i[5])
+
+        goal_difference = int(goals_home) - int(goals_away)
+
+        if winner == '1':
+            results = rating_update(expected_outcome(teams[home], teams[away])[1], 1, teams[home], teams[away], KVAL, goal_difference)
+            teams[home] = results[0]
+            teams[away] = results[1]
+
+        if winner == '0':
+            results = rating_update(expected_outcome(teams[away], teams[home])[1], 0, teams[away], teams[home], KVAL, goal_difference)
+            teams[home] = results[1]
+            teams[away] = results[0]
+        
+    print("The model has predicted %s games correctly and %s games incorrectly" %(correct,wrong))
+    print("The result is a %s" %(correct/(correct+wrong)))
 
 def main():
+    KVAL = 20
+
     file_path = 'C:/Users/codym/OneDrive - University of Calgary/Desktop/Econ 411 - Computer Applications/soccer data/soccer_data2.csv'
     with open(file_path, 'r', newline = '') as csv_f:
         data = list(csv.reader(csv_f))
@@ -88,7 +128,7 @@ def main():
         if i[2] not in teams:
             teams[i[2]] = 1200
 
-    print(run_games(data, teams))
-    outcome_pr(teams)
+    print(run_games(data, teams, KVAL))
+    ML_prediction(data, teams, KVAL)
 
 main()
