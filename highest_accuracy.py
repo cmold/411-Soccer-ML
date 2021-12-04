76f76eaff2091fde7c17104ee7aef7836e6ecdae
Cody <90341326+euthy0@users.noreply.github.com>
2021-12-04 01:35:41 -0700

    Update highest_accuracy.py

--- highest_accuracy.py
+++ highest_accuracy.py
@@ -1,41 +1,55 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.type_check import real
+import pandas as pd
 
 # https://en.wikipedia.org/wiki/Elo_rating_system#Theory
 
+def column_find(data):
+    """
+    Find location of column headers, allowing for changing location of columns
 
-def expected_outcome(ratingA, ratingB):
+    Args:
+        data: csv data
+
+    Returns:
+       col_loc: dictionary of column names and their numeric location
+    """
+    n = 0
+    cols = ['HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR', 'HS', 'AS', 'HST', 'AST', 'HF', 'AF', 'HC', 'AC']
+    col_loc = {}
+    for i in data[0]:
+        for j in cols:
+            if i == j:
+                col_loc[j] = n 
+        n += 1
+    return(col_loc)
+
+def expected_outcome(winner, loser):
     """
     Estimate outcome of two teams given their rating
 
     Args:
-        ratingA (float): Rating for team A
-        ratingB (foat): Rating for team B
+        winner (float): Rating for winning team
+        loser (foat): Rating for losing team
 
     Returns:
         float: Returns expected value of winner or 0.5 if draw
     """
-    expectedA = (1/(1+10**((ratingB - ratingA)/400)))
-    expectedB = (1/(1+10**((ratingA - ratingB)/400)))
+    expectedWin = (1/(1+10**((loser - winner)/400)))
+    expectedLoss = (1/(1+10**((winner - loser)/400)))
+    expectedDraw = expectedWin - expectedLoss
 
-    if expectedA > expectedB:
-        return expectedA
-    
-    if expectedA < expectedB:
-        return expectedB
-    
-    if expectedA == expectedB:
-        return 0.5
+    return(expectedWin, expectedLoss, expectedDraw)
 
 
-def rating_update(expected_outcome, actual_outcome, winner, loser, KVAL, ind_variables, gd_mult):
+def rating_update(expected_outcome, winner, loser, winner_kval, loser_kval, ind_variables):
     """
     Updates team ratings after a match
 
     Args:
         expected_outcome (float): Expected match outcome based on ratings
-        actual_outcome (int): Actual match outcome
         winner (string): The team who won
         loser (string): The team who lost
         KVAL (int): 
@@ -45,64 +59,86 @@ def rating_update(expected_outcome, actual_outcome, winner, loser, KVAL, ind_var
     Returns:
         float: Returns both updated team ratings
     """
-    
-    winner_rating = (winner - (gd_mult*KVAL)*(actual_outcome - expected_outcome) + 
-    ((gd_mult*KVAL) * abs(ind_variables['Goal Difference'])) -
-    ((0.0657*KVAL) * ind_variables['Foul Difference']) + 
-    ((0.0867*KVAL) * ind_variables['Shots Taken']) +
-    ((0.0638*KVAL) * ind_variables['Corners']))
+
+    winner_rating = (winner + (winner_kval)*(expected_outcome[0]) + 
+    (0.40*ind_variables['Goal Difference']) -
+    (0.20*ind_variables['Foul Difference']) + 
+    (0.20*ind_variables['Shots Taken']) +
+    (0.20*ind_variables['Corners']))
 
     # Subtract variable factors
     # e.g. if goal diff is negative, away team won, so the second line will add to their rating
     # and take away from home teams rating
-    loser_rating = (loser - (gd_mult*KVAL)*((1-actual_outcome) - abs(expected_outcome - 1)) - 
-    ((gd_mult*KVAL) * ind_variables['Goal Difference']) + 
-    ((0.0657*KVAL) * ind_variables['Foul Difference']) + 
-    ((0.0867*KVAL) * ind_variables['Shots Taken']) -
-    ((0.0638*KVAL) * ind_variables['Corners']))
+    loser_rating = (loser - (loser_kval)*(expected_outcome[1]) - 
+    (0.40*ind_variables['Goal Difference']) + 
+    (0.20*ind_variables['Foul Difference']) - 
+    (0.20*ind_variables['Shots Taken']) -
+    (0.20*ind_variables['Corners']))
 
     return(winner_rating, loser_rating)
 
 
-def run_games(data, teams, KVAL, ind_variables):
+def run_games(data, teams, cols, KVAL, ind_variables):
     """
     Takes sample data to create elo model
 
     Args:
         data (csv): CSV file
         teams (dict): Dictionary with variable name as key and coefficient as value
+        columns (dict): 
         KVAL (int): 
         ind_variables (dict): Dictionary with variable name as key and coefficient as value
 
     Returns:
         dict: Returns both updated team ratings
     """
+
     # Loop through each data point (game)
     # skip last 300 as they are used for testing purposes
     for i in data[1:len(data) - 300]:
-        home = i[2]
-        away = i[3]
-        winner = i[6]
+        home = i[cols['HomeTeam']]
+        away = i[cols['AwayTeam']]
+        winner = i[cols['FTR']]
 
-        ind_variables['Corners'] = int(i[17]) - int(i[18])
-        ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
-        ind_variables['Foul Difference'] = int(i[15]) - int(i[16])
-        ind_variables['Goal Difference'] = int(i[4]) - int(i[5])
 
+        team_kval = [0,0]
+        n = 0
+        # Determines the kval each team gets based on their elo range
+        for j in (teams[home], teams[away]):
+            if j < 2100:
+                team_kval[n] = KVAL[0]
+            elif j in range(2100, 2400):
+                team_kval[n] = KVAL[1]
+            elif j > 2400:
+                team_kval[n] = KVAL[2]
+            n += 1
+
+        # Calculate the independent variable differences
+        ind_variables['Corners'] = int(i[cols['HC']]) - int(i[cols['AC']])
+        ind_variables['Shots Taken'] = int(i[cols['HST']]) - int(i[cols['AST']])
+        ind_variables['Foul Difference'] = int(i[cols['HF']]) - int(i[cols['AF']])
+        ind_variables['Goal Difference'] = int(i[cols['FTHG']]) - int(i[cols['FTAG']])
+
+        # Find results if winner is the home team
         if winner == '1':
-            results = rating_update(expected_outcome(teams[home], teams[away]), 1, teams[home], teams[away], KVAL, ind_variables)
+            winner_kval = team_kval[0]
+            loser_kval = team_kval[1]
+            results = rating_update(expected_outcome(teams[home], teams[away]), teams[home], teams[away], winner_kval, loser_kval, ind_variables)
             teams[home] = results[0]
             teams[away] = results[1]
 
+        # Find results if winner is the away team
         if winner == '0':
-            results = rating_update(expected_outcome(teams[away], teams[home]), 0, teams[away], teams[home], KVAL, ind_variables)
+            winner_kval = team_kval[1]
+            loser_kval = team_kval[0]
+            results = rating_update(expected_outcome(teams[away], teams[home]), teams[away], teams[home], winner_kval, loser_kval, ind_variables)
             teams[home] = results[1]
             teams[away] = results[0]
 
     return(teams)
 
 
-def outcome_pr(teams, home_team, away_team):
+def outcome_pr(teams, home_team, away_team, gap, home_advantage):
     """
     Compares two teams
 
@@ -110,27 +146,28 @@ def outcome_pr(teams, home_team, away_team):
         teams (dict): Dictionary with variable name as key and coefficient as value
         home_team (string): The home team 
         away_team (string): The away team
+        gap (int): the upper and lower bound of elos for a game to be a draw
 
     Returns:
         string: The match result
     """
     # Check the elo of the teams and weight the winner with a range for draws
-    if teams[home_team] > teams[away_team]:
+    if teams[home_team] + home_advantage > teams[away_team] + gap:
         return(home_team)
     
-    elif teams[home_team] < teams[away_team]:
+    elif teams[home_team] + home_advantage < teams[away_team] - gap:
         return(away_team)
-    else:
-        return('Draw')
+    
 
 
-def ML_prediction(data, teams, KVAL, gd_mult):
+def ML_prediction(data, teams, cols, KVAL, GAP, HOME_ADVANTAGE):
     """
     Uses our model in predicting soccer game outcomes
 
     Args:
         data (list): The data for soccer matches
         teams (dict): Dictionary with variable name as key and coefficient as value
+        cols (dict):
         KVAL (int): 
         gd_mult (float): Goal Difference multiplier
 
@@ -144,124 +181,115 @@ def ML_prediction(data, teams, KVAL, gd_mult):
     real_outcome = ''
     ind_variables = {}
 
+    home_but_away = 0
+    away_but_home = 0
+    draw_but_not = 0
+    notdraw_but_draw = 0
+
     # Looks at the last 300 games in our data set
     for i in data[len(data) - 300:]:
-        home = i[2]
-        away = i[3]
-        winner = i[6]
+        home = i[cols['HomeTeam']]
+        away = i[cols['AwayTeam']]
+        winner = i[cols['FTR']]
 
         if winner == '1': real_outcome = home
         elif winner == '0': real_outcome = away
         elif winner == '0.5': real_outcome = 'Draw'
 
         # Get our models predicted winner
-        prediction = outcome_pr(teams, home, away)
+        prediction = outcome_pr(teams, home, away, GAP, HOME_ADVANTAGE)
 
         # Keep track of # of correct and wrong
         if prediction == real_outcome: correct += 1
-        else: wrong += 1
+        else: 
+            wrong += 1
+            if prediction == home and real_outcome == away:
+                home_but_away += 1
+            if prediction == away and real_outcome == home:
+                away_but_home += 1
+            if prediction == 'Draw' and real_outcome in (home, away):
+                draw_but_not += 1
+            if prediction in (home, away) and real_outcome == 'Draw':
+                notdraw_but_draw += 1
+
             
         # The rest of this section adds updates the ELO rating based on the current game
 
-        ind_variables['Corners'] = int(i[17]) - int(i[18])
-        ind_variables['Shots Taken'] = int(i[13]) - int(i[14])
-        ind_variables['Foul Difference'] = int(i[15]) - int(i[16])
-        ind_variables['Goal Difference'] = int(i[4]) - int(i[5])
+        team_kval = [0,0]
+        n = 0
+        for j in (teams[home], teams[away]):
+            if j < 2100:
+                team_kval[n] = KVAL[0]
+            elif j in range(2100, 2400):
+                team_kval[n] = KVAL[1]
+            elif j > 2400:
+                team_kval[n] = KVAL[2]
+            n += 1
+
+        ind_variables['Corners'] = int(i[cols['HC']]) - int(i[cols['AC']])
+        ind_variables['Shots Taken'] = int(i[cols['HST']]) - int(i[cols['AST']])
+        ind_variables['Foul Difference'] = int(i[cols['HF']]) - int(i[cols['AF']])
+        ind_variables['Goal Difference'] = int(i[cols['FTHG']]) - int(i[cols['FTAG']])
         
         if winner == '1':
-            results = rating_update(expected_outcome(teams[home], teams[away]), 1, teams[home], teams[away], KVAL, ind_variables, gd_mult)
+            winner_kval = team_kval[0]
+            loser_kval = team_kval[1]
+            results = rating_update(expected_outcome(teams[home], teams[away]), teams[home], teams[away], winner_kval, loser_kval, ind_variables)
             teams[home] = results[0]
             teams[away] = results[1]
 
         if winner == '0':
-            results = rating_update(expected_outcome(teams[away], teams[home]), 0, teams[away], teams[home], KVAL, ind_variables, gd_mult)
+            winner_kval = team_kval[1]
+            loser_kval = team_kval[0]
+            results = rating_update(expected_outcome(teams[away], teams[home]), teams[away], teams[home], winner_kval, loser_kval, ind_variables)
             teams[home] = results[1]
             teams[away] = results[0]
     
+    print('Predicted Home but Away: ' + str(round(home_but_away/wrong, 4)) + '%')
+    print('Predicted Away but Home: ' + str(round(away_but_home/wrong, 4)) + '%')
+    print('Predicted Draw but Not: ' + str(round(draw_but_not/wrong, 4)) + '%')
+    print('Predicted Not Draw but Draw: ' + str(round(notdraw_but_draw/wrong, 4)) + '%')
     res1 = "The model has predicted %s games correctly and %s games incorrectly out of %s total games" %(correct, wrong, correct+wrong)
     accuracy = np.round(correct/(correct+wrong), 4)
-    return(res1, accuracy, gd_mult)
+    return(res1, accuracy)
 
+def graph_output(teams):
+    data = []
+    for i in teams.values():
+        data.append(int(i))
 
-def optimize(param, accuracy):
-    """
-    Finds the optimal multiplier for a parameter
-
-    Args:
-        param (list): The values of the parameters
-        accuracy (list): The accuracy scores that correspond to the parameters
-
-    Returns:
-        float: The optimal parameter for maximum accuracy
-    """
-    param_array = np.array(param)
-    accuracy_array = np.array(accuracy)
-
-    # Finds index with highest accuracy
-    optimal_index = np.argmax(accuracy_array)
-    return param_array[optimal_index]
-
-
-def plot_optimal(param, accuracy):
-    """
-    Plots a line graph for different values of a parameter and model accuracy
-
-    Args:
-        param (list): The values of the parameters
-        accuracy (list): The accuracy scores that correspond to the parameters
-    """
-    plt.figure(figsize=(10,6))
-    plt.plot(param, accuracy)
+    min_val = min(data)
+    max_val = max(data)
     
-    plt.title('Goal Difference Multiplier Affect on Accuracy', 
-              fontsize=12)
-    plt.xlabel('Multiplier', fontsize=12)
-    plt.ylabel('Accuracy', fontsize=12)
-    
-    plt.savefig('GD_Acc.jpg')
+    plt.hist(data, bins=range(min_val, max_val + 75, 75), edgecolor="k")
+    #plt.xticks(bins)
     plt.show()
 
-
 def main():
-    KVAL = 15
+    # KVAL for elo ranges, below 2100, 2100-2400, above 2400
+    KVAL = [32, 24, 16]
+    # Elo range for predicting draws
+    GAP = 0
+    # Bonus elo for a home team
+    HOME_ADVANTAGE = 150
     ind_variables = {}
 
-    file_path = 'github_clone/soccer_data2.csv'
+    file_path = 'C:/Users/codym/OneDrive - University of Calgary/Desktop/Econ 411 - Computer Applications/soccer data/soccer_data2.csv'
     with open(file_path, 'r', newline = '') as csv_f:
         data = list(csv.reader(csv_f))
+
+    cols = column_find(data)
 
     # Get all teams in the csv file and give them a base elo rating of 1200
     teams = {}
     for i in data[1:]:
-        if i[2] not in teams:
-            teams[i[2]] = 1200
-    
-    # print(run_games(data, teams, KVAL, ind_variables))
-    
-    gd_optimize_list = []
-    accuracy_list = []
-    gd_vals = np.arange(0, 1, 0.002)  # (start, end, step)
-    
-    # Find optimal goal difference estimate
-    for gd_mult in gd_vals:
-        res1, accuracy, gd_mult = ML_prediction(data, teams, KVAL, gd_mult)
-        gd_optimize_list.append(gd_mult)
-        accuracy_list.append(accuracy)
-    
-    gd_optimize_array = np.array(gd_optimize_list)
-    accuracy_array = np.array(accuracy_list)
-    
-    plot_optimal(gd_optimize_array, accuracy_array)
-    
-    # Hyperparameter tuning
-    gd_optimal = optimize(gd_optimize_array, accuracy_array)
-    
-    # Get optimized results
-    res1, accuracy, gd_mult = ML_prediction(data, teams, KVAL, gd_optimal)
-    accuracy = np.round(accuracy*100, 4)
-    
-    print(res1)
-    print('Model accuracy: %s percent' %(accuracy))
+        if i[cols['HomeTeam']] not in teams:
+            teams[i[cols['HomeTeam']]] = 1200
+        if i[cols['AwayTeam']] not in teams:
+            teams[i[cols['AwayTeam']]] = 1200
 
+    print(run_games(data, teams, cols, KVAL, ind_variables))
+    print(ML_prediction(data, teams, cols, KVAL, GAP, HOME_ADVANTAGE))
 
+    graph_output(teams)
 main()
